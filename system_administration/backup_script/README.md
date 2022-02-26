903e5ca88c73f31ae5d1abcce5d89152506cf9cd
angry-byte <contact@angry-byte.com>
2022-02-26 07:08:58 +0300

    Added switches to backup script

--- system_administration/backup_script/README.md
+++ system_administration/backup_script/README.md
@@ -1,17 +1,24 @@
 ## Description
 
-This is a Python3 script that copies a directory and then compresses the directory into zip file that is named by the current date on the operating system. The script can be modified to prompt for a password that would protect the zip file.
-
-The archive is created with the date and time as the name of the file. The format is: yyyy-mm-dd HH-MM-SS
+This is a Python3 script that copies a directory and then compresses the directory into zip file that is named by the current date on the operating system. The archive is created with the date and time as the name of the file. The file name format is: yyyy-mm-dd HH-MM-SS.zip
 
 IMPORTANT: This script does not encrypt the zip archive using a password. To mitigate this security risk, set restrictive file permissions to prevent others from extracting it.
+
+
+## How to Use
+
+Execute the script using python. If you want to backup a single folder, use the -b or --backup switch. If you want to backup multiple folders, use the -m or --multiple switch and you will be prompted to enter the locations of all the folders you want to backup. 
+
+If the folder is in the same directory as the script, just enter the folder name. Otherwise, you will need to provide the full path of the folder.
+
 
 ## Tested Platforms:
 
 * Cinnamon Mint 20.3
 * Windows 10
 
+
 ## Future Plans and Ideas
 
-- [ ] Add switches to be enabled for manual backups
-- [ ] Add password implementation, preferably with a native library
+- [x] Add switches to be enabled for manual backups
+- [ ] Implement zip password protection, preferably with a native library
--- system_administration/backup_script/script.py
+++ system_administration/backup_script/script.py
@@ -1,10 +1,93 @@
 import datetime
 import shutil
+import getopt
+import sys
+import os
+import string
+
+
+def help():
+    print(f'Project Link: https://github.com/angry-byte/' +
+            'Python3-Projects/tree/main/system_administration/backup_script')
+    print(f'')
+    print(f'Options:                Description:')
+    print(f'-b      --backup        select a single folder to backup')
+    print(f'-h      --help          print out the help page')
+    print(f'-m      --multiple      enable a prompt for backing up multiple folders')
+
+    exit(0)
+
+
+def backup_function(target_directory):
+    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    shutil.make_archive(current_date_time, 'zip', target_directory)
+
+    print(f'[!] Name of backup file created: {current_date_time + ".zip"}')
+
+
+def main(target_folder, multiple_condition):
+    first_folder_prompt = True
+
+    if multiple_condition:
+        folders = []
+        tmp_folder = os.mkdir('tmp_backup_folder')
+
+        while True:
+            try: 
+                folder_count = int(input('Enter the number of folders you want to backup: '))
+                break
+            except:
+                print(f'Error: you must enter a digital number for the number ' +
+                        'of digital folders you want to backup')
+
+        for folder in range(folder_count):
+            if first_folder_prompt:
+                folder_location = input('Enter the location of the folder to backup: ')
+                print(f'')
+                folders.append(folder_location)
+                first_folder_prompt = False
+            else:
+                folder_location = input('Enter the location of another folder: ')
+                print(f'')
+                folders.append(folder_location)
+        
+        print(f'[!] Copying folders to backup...')
+        for folder in folders:
+            if folder[0] in string.ascii_uppercase and folder[1] == ':':
+                folder = folder[2:]
+
+            try:
+                shutil.copytree(folder, 'tmp_backup_folder' + '/' + folder)
+            except FileNotFoundError:
+                print(f'[-] Error: file {folder} was not found.')
+                print(f'[!] Continuing backup process...')
+                print(f'')
+
+
+        print(f'[!] Creating backup...')
+        backup_function('tmp_backup_folder')
+        shutil.rmtree('tmp_backup_folder')
+
+    else:
+        backup_function(target_folder)
+
+    print(f'[+] Backup process complete!')
+    exit(0)
 
 
 if '__main__' == __name__:
-    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    target_directory = input('Enter the name/full path of the folder to copy and archive: ')
+    options_list = sys.argv[1:]
+    short_options = 'b:hm'
+    long_options = ['backup =', 'help', 'multiple']
+    options, values = getopt.getopt(options_list, short_options, long_options)
 
-    shutil.make_archive(current_date_time, 'zip', target_directory)
-
+    for given_argument, given_value in options:
+        if given_argument in ('-h', '--help'):
+            help()
+        elif given_argument in ('-b', '--backup'):
+            target_folder = given_value
+            main(given_value, False)
+        elif given_argument in ('-m', '--multiple'):
+            main('', True)
+    
+    print(f'[!] Program exited without doing anything. Did you use a switch?')
