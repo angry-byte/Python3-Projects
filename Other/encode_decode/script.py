af97cb3c95a7f203ce6d920db66c0efd5e13582c
angry-byte <contact@angry-byte.com>
2022-02-27 03:27:25 +0300

    Cleaned code in encode_decode project

--- Other/encode_decode/script.py
+++ Other/encode_decode/script.py
@@ -14,54 +14,26 @@ def start():
 
 
 def decode_string(given_string):
-    decode_condition = False
     given_string = given_string.encode()
-    options = {'1': 'base16', '2': 'base32', '3': 'base64', '4': 'base85'}
+    functions_list = [base64.b16decode, base64.b32decode, base64.b64decode, base64.a85decode]
+    options = {0: 'base16', 1: 'base32', 2: 'base64', 3: 'base85'}
 
     print(f'Decoding...')
-    while True:
-        try:
-            result = base64.b16decode(given_string)
-            scheme_used = '1'
-            decode_condition = True
-            break
-        except:
-            decode_condition = False
-        try:
-            result = base64.b32decode(given_string)
-            scheme_used = '2'
-            decode_condition = True
-            break
-        except:
-           decode_condition = False
-        try:
-            result = base64.b64decode(given_string)
-            scheme_used = '3'
-            decode_condition = True
-            break
-        except:
-            decode_condition = False
-        try:
-            result = base64.a85decode(given_string)
-            scheme_used = '4'
-            decode_condition = True
-            break
-        except:
-            decode_condition = False
-            break
 
-    if decode_condition:
-        for i in options:
-            if i == scheme_used:
-                print(f'[+] The encoding scheme used on the string was: {options[i]}')
-        result = result.decode()
-        print(f'The result of the string decoded is: {result}')
+    for i, scheme in enumerate((functions_list)):
+        try:
+            result = scheme(given_string)
+            print(f'[+] String decoded. The decoded string is: {result.decode()}')
+            print(f'[+] The decoding scheme used was: {options[i]}')
+            break
+        except:
+            continue
     else:
-        print(f'[-] Couldn\'t decode string. Doesn\'t look to be base encoded.')
+        print(f'[-] Could not decode the string. Doesn\'t look to be base encoded.')
 
 
 def encode_string(given_string):
-    options = ['1', '2', '3', '4']
+    options = {1: base64.b16encode, 2: base64.b32encode, 3: base64.b64encode, 4: base64.a85encode}
     given_string = given_string.encode()
 
     print(f'Options: ')
@@ -69,26 +41,19 @@ def encode_string(given_string):
     print(f'2) base32')
     print(f'3) base64')
     print(f'4) base85')
-    user_select = input('Select the encoding scheme you want to use: ')
+    user_select = int(input('Select the encoding scheme you want to use: '))
     
-    while user_select not in options:
+    while user_select not in options.keys():
         print(f'Error. Incorrect option.')
         user_select = input('Select the encoding scheme you want to use: ')
 
-    if user_select == '1':
-        converted_string = base64.b16encode(given_string)
-    elif user_select == '2':
-        converted_string = base64.b32encode(given_string)
-    elif user_select == '3':
-        converted_string = base64.b64encode(given_string)
-    elif user_select == '4':
-        converted_string = base64.a85encode(given_string)
-        
-
-    resulting_string = converted_string.decode()
-
-    print(f'')
-    print(f'[+] String has been encoded. Result: {resulting_string}')
+    for i, scheme in enumerate((options.values())):
+        if (i) == (user_select - 1):
+            result = scheme(given_string).decode()
+            print(f'[+] String has been encoded. Result: {result}')
+            break
+        else:
+            continue
 
 
 if __name__ == "__main__":
@@ -102,3 +67,4 @@ if __name__ == "__main__":
         user_string = input('Enter the string of data you want to encode: ')
         print(f'')
         encode_string(user_string)
+
